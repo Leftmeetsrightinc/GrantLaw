e3d1ec729c530e272e3b774a67d1cb672caf821b
lavanyaks <lavanya.singh24@gmail.com>
2018-02-04 13:00:12 -0500

    Styling fixings and changes based on feedback

--- content.php
+++ content.php
@@ -1,18 +1,17 @@
 <article <?php post_class(); ?>>
 
+  <div class="article-content">
     <?php if (the_post_thumbnail()) the_post_thumbnail(); ?>
 
-    <div class="article-content">
-
     <?php if (!is_front_page()) : ?>
-      <h1 class="blog-post-title"><?php the_title(); ?></h1>
+    <h1 class="blog-post-title"><?php the_title(); ?></h1>
     <?php endif; ?>
-      <div class="blog-all-content">
-        <p class="blog-post-meta"><?php the_time('F, Y') ?></p>
-        
-        <div class="blog-text-content">
-          <?php the_content(); ?>
-        </div>
+    <div class="blog-all-content">
+      <p class="blog-post-meta"><?php the_time('F, Y') ?></p>
+
+      <div class="blog-text-content">
+        <?php the_content(); ?>
       </div>
     </div>
-  </article>
+  </div>
+</article>
--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 		<footer class="blog-footer">
       <div>
         <span class="copyright">Copyright © 2018 Grant Law LLP All rights reserved</span>
-        <span class="made-by">Made with love by </span><a href="https://leftmeetsrightinc.com">Left meets Right Inc.</a>
+        <span class="made-by">Made with love by <a href="https://leftmeetsrightinc.com">Left meets Right Inc.</a></span>
       </div>
     </footer>
     <script src="https://use.fontawesome.com/c4a772538f.js"></script>
