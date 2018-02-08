0bfdbdc29a07b347773be0e1027cfe609bd21e99
lavanyaks <lavanya.singh24@gmail.com>
2018-02-07 20:17:03 -0500

    Adding photo credit styling and content

--- content.php
+++ content.php
@@ -3,6 +3,8 @@
   <div class="article-content">
     <?php if (the_post_thumbnail()) the_post_thumbnail(); ?>
 
+    <span class="photo-credit">Photo credit: <?php echo get_post(get_post_thumbnail_id())->post_excerpt; ?></span>
+
     <?php if (!is_front_page()) : ?>
     <h1 class="blog-post-title"><?php the_title(); ?></h1>
     <?php endif; ?>
